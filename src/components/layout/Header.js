16c96f7b65f25e6e61569426ebdce51375e2158f
박정인 <jeongin@hivelab.co.kr>
2024-08-23 19:08:29 +0900

    map -> headerinfo 생성

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+
+const headerInfo = [
+  {info : "챔피언"},
+  {info : "main"},
+  {info : "고르기"},
+]
+
+function Info({data}){
+  return <div>{data}</div>;
+}
+
 function Header(){
   return (
     <header className="header">
-      <div className="header_list">헤더입니다.</div>
+      <div className="header_list">
+        {headerInfo.map((headerText) => (
+          <Info data={headerText.info} key={headerText.info} />
+        ))}
+      </div>
     </header>
   )
 }
