e79bef3af7ee89707852a9ee0f6a89197bf77ed7
박정인 <jeongin@hivelab.co.kr>
2024-08-23 18:26:54 +0900

    router 설정 -> Product API 세팅

--- src/Service/ProductAPI.js
+++ src/Service/ProductAPI.js
@@ -1,6 +1,7 @@
 // ProductAPI.js
 import axios from "axios";
 import gsap from "gsap";
+import { BrowserRouter } from "react-router-dom";
 
 export const ProductAxios = async () => {
   const AxiosURL = `{}`
@@ -11,5 +12,11 @@ export const ProductAxios = async () => {
 export const ProductGsap = async () => {
   const GsapURL = `{}`
   const response = await gsap.get(GsapURL)
+  return response
+}
+
+export const ProductRouter = async () => {
+  const RouterURL = `{}`
+  const response = await BrowserRouter.get(RouterURL)
   return response
 }
