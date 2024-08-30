38b0d6f9e9bbabb03ba49927640594c181dd007b
박정인 <jeongin@hivelab.co.kr>
2024-08-30 12:19:23 +0900

    feat: main video 추가

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,10 +1,43 @@
 import React from "react";
 import Header from "../layout/Header";
+import Pentagon from "../../assets/img/pentagon_border.png";
+import Video from "../../assets/videos/background_video.webm";
+
+const VideoCont = [
+  {
+    className : "background_video",
+    type : "video/webm"
+  }, 
+  {
+    className : "center_video",
+    type : "video/webm"
+  },
+]
+
 
 function Main(){
   return (
     <>
       <Header />
+      <div className="main_video_wrap">
+        <div className="pentagon">
+          <img src={Pentagon} alt="" />
+        </div>
+        {VideoCont.map((e) => (
+          <video
+            muted
+            autoPlay
+            loop
+          >
+            <source 
+              className = {e.className}
+              src = {Video}
+              type = {e.type}
+            >
+            </source>
+          </video>
+        ))}
+      </div>
     </>
   )
 }
